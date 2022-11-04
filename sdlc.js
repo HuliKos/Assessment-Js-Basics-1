92339743302ad33ffa719c3e74f80aa6a8b69e15
Lan Kostrikin <lankostrikin@gmail.com>
2022-11-04 13:47:32 -0500

    Final version

--- sdlc.js
+++ sdlc.js
@@ -19,8 +19,8 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
-const analysisOfRequirements =
-const design =
-const implementation =
-const testingAndIntegration =
-const maintain = 
+const analysisOfRequirements = `The analysis stage includes gathering all the specific details required for a new system as well as determining the first ideas for prototypes.`
+const design = `The design stage is a necessary precursor to the main developer stage. Developers will first outline the details for the overall application, alongside specific aspects. Once complete, development managers will prepare a design document to be referenced throughout the next phases of the SDLC.`
+const implementation = `The development/implementation stage is the part where developers actually write code and build the application according to the earlier design documents and outlined specifications.`
+const testingAndIntegration = `The developed code needs to be tested to make sure that there are no any bugs and that the end-user experience will not negatively be affected at any point. Once the testing is performed, different modules or designs will be ingtegrated into the primary source code through developer efforts, usually by leveraging training environments to detect future errors and defects.`
+const maintain = `Developers must now move into a maintenance mode and begin practicing any activities required to handle issues reported by end-users. Developers are also responsible for implementing any changes that the software might need after deployment.`
