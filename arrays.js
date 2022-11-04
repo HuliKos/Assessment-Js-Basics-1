c0a7a685871791745f0bff5c3f0ae23588c41c45
Lan Kostrikin <lankostrikin@gmail.com>
2022-11-04 14:08:22 -0500

    Final version

--- arrays.js
+++ arrays.js
@@ -3,23 +3,27 @@
 
 const faveColors = ['red', 'green', 'black']
 
-//CODE HERE
+var colorCopy = faveColors.slice(0,3)
+
+// console.log(colorCopy)
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
-//CODE HERE
+colorCopy.push(`blue`)
+
 
 //////////////////PROBLEM 3////////////////////
 /*
-  Using an array method, create a new array called 'middleNums' from the 
-  numbers array that will capture only the middle numbers (2, 3, 4).
+Using an array method, create a new array called 'middleNums' from the 
+numbers array that will capture only the middle numbers (2, 3, 4).
 */
 
 const numbers = [1, 2, 3, 4, 5]
 
-//CODE HERE
+var middleNums = numbers.slice(1,4)
 
+// console.log(middleNums)
 
 //////////////////PROBLEM 4////////////////////
 /* 
@@ -33,4 +37,12 @@ const numbers = [1, 2, 3, 4, 5]
 
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
-// CODE HERE
+var answers =[]
+
+for(i=0; i < bigOrSmallArray.length; i++){
+  if(bigOrSmallArray[i] > 100){
+    answers.push(bigOrSmallArray[i])
+  } else {answers.push(`small`)}
+}
+
+// console.log(answers)
